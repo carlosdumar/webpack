01770f5f5db6f70015fc524487db0d603bf46d33
Carlos Andres Rodriguez <cdumar9@hotmail.com>
2018-05-22 22:14:14 -0500

    Se agrego loader-fonts

--- url-loader-images/index.js
+++ url-loader-images/index.js
@@ -8,8 +8,8 @@ delayedMessage();
 
 const img = document.createElement('img');
 img.setAttribute('src', platziImg);
-img.setAttribute('width', 50);
-img.setAttribute('height', 50);
+img.setAttribute('width', 100);
+img.setAttribute('height', 100);
 document.body.append(img);
 
 console.log('Hola mundo!, desde webpack en un webpack.config');
